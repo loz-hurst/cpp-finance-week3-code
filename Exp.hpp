3165da329460ddb179105ed9097e494f70643b00
Laurence <laurence+git@entek.org.uk>
2019-10-20 11:49:34 +0100

    Added exponential function

---
+++ Exp.hpp
@@ -0,0 +1,32 @@
+/*
+ * Exponential functions for week 3 exercises of C++ for Finance.
+ *
+ * Copyright 2019 Laurence Alexander Hurst
+ *
+ * This file is part of C++ for Finance.
+ *
+ *     C++ for Finance is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     C++ for Finance is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * See the file LICENCE in the original source code repository for the
+ * full licence.
+ */
+#ifndef WEEK_3_EXP_HPP
+#define WEEK_3_EXP_HPP
+
+namespace mathutils {
+    // Approximate e^x using n terms of the Taylor series
+    double exp(double x, double n);
+}
+
+#endif //WEEK_3_EXP_HPP
